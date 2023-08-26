e0f6ed5e02227edf7e2ca2dd88bb20f66facec3a
Soon Jae <ryupark1321@gmail.com>
2023-08-26 19:42:18 -0400

    Deleted unnecessary files

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryupark1321/Desktop/Exchange
-# Build directory: /home/ryupark1321/Desktop/Exchange
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
